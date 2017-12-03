c9100d4bd2ee64273767f02489fe23796893398c
Koen <penningmeester@badminton-pbo.be>
2017-12-03 15:18:33 +0100

    Upto excercise 3

--- src/org/adventOfCode/code2017/Excercise2.java
+++ src/org/adventOfCode/code2017/Excercise2.java
@@ -3,7 +3,10 @@ package org.adventOfCode.code2017;
 import org.adventOfCode.GenericExcercise;
 import org.adventOfCode.util.FileUtil;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.ToLongFunction;
+import java.util.stream.Collectors;
 
 public class Excercise2 extends GenericExcercise {
 
@@ -11,14 +14,62 @@ public class Excercise2 extends GenericExcercise {
     public void executeTest() throws Exception {
 
         List<String> input = FileUtil.readStringsFromFile("resource/2017/Excercise2.txt");
-/*
-        long result = calculateCaptcha(input.get(0));
-        logger.debug("Result of excercise 1A: "+result);
 
-        result = calculateCircularCaptcha(input.get(0));
-        logger.debug("Result of excercise 1B: "+result);*/
+        long result = calculateChecksum(input);
+        logger.debug("Result of excercise 2A: "+result);
+
+        result = calculateEvenChecksum(input);
+        logger.debug("Result of excercise 2B: "+result);
 
     }
+
+
+    public long calculateChecksum(List<String> input)
+    {
+        long result = 0;
+
+        for(String s : input)
+        {
+           List<Integer> intList = Arrays.stream(s.split("\\s")).map( i -> Integer.parseInt(i))
+                                                                    .sorted()
+                                                                    .collect(Collectors.toList());
+           result += intList.get(intList.size()-1) - intList.get(0);
+        }
+
+        return result;
+    }
+
+    public long calculateEvenChecksum(List<String> input)
+    {
+        long result = 0;
+
+        for(String s : input)
+        {
+            List<Integer> intList = Arrays.stream(s.split("\\s")).map( i -> Integer.parseInt(i))
+                    .sorted()
+                    .collect(Collectors.toList());
+
+
+            for(int i = 0; i < intList.size(); i++)
+            {
+                int div = 0;
+
+                for(int j = i+1; j < intList.size();j++)
+                {
+                    if(intList.get(j)%intList.get(i) == 0)
+                    {
+                        div = intList.get(j)/intList.get(i);
+                        result += div;
+                        break;
+                    }
+                }
+                if(div > 0) break;
+            }
+        }
+
+        return result;
+    }
+
 
 
 }
--- test/org/adventOfCode/code2017/Excercise2Test.java
+++ test/org/adventOfCode/code2017/Excercise2Test.java
@@ -1,6 +1,9 @@
 package org.adventOfCode.code2017;
 
+import org.adventOfCode.util.FileUtil;
 import org.junit.Test;
+
+import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -10,9 +13,14 @@ public class Excercise2Test {
     public void testResult() throws Exception {
         Excercise2 test = new Excercise2();
 
+        List<String> input = FileUtil.readStringsFromFile("resource/2017/Excercise2Test.txt");
 
-      //  long result = test.calculateCaptcha("1122");
-      //  assertTrue("Captha of 1122  expect 3 "+"but found " + result,result == 3);
+        long result = test.calculateChecksum(input);
+        assertTrue("checksum expect 18 "+"but found " + result,result == 18);
+
+        input = FileUtil.readStringsFromFile("resource/2017/Excercise2BTest.txt");
+        result = test.calculateEvenChecksum(input);
+        assertTrue("checksum expect 9 "+"but found " + result,result == 9);
 
         test.executeTest();
     }
